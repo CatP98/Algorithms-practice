1c4d06ef2d6bf3af84a5cb77f1e44ab7c1d5b5e0
catarina p <catarinampestana@ua.pt>
2024-10-17 16:51:21 +0100

    Slidding window pattern

---
+++ algorithms/practices/resources/patterns/sliding-window/max-sum.js
@@ -0,0 +1,38 @@
+/*
+Write a function called maxSunarraySum that accepts an array opf integers and a numbet called n.
+The function should calculate the maximu sum of n consecutive Telements in the array
+
+maxSubarraySum([1,2,5,2,8,1,5]. 2);  // 10 (2+8 )
+maxSubarraySum([1,2,5,2,8,1,5], 4);  // 17 (2+5+2+8)
+maxSubarraySum([4,2,1,6], 1); // 6
+maxSubarraySum([4,2,1,6,2], 4); // 13 (4+2+1+6)
+maxSubarraySum([], 4); // null
+ */
+
+
+function maxSubarraySum(arr, n){
+    // Case the array is empty, return 0
+    if( arr.length < n || arr.length === 0){
+        return null;
+    }
+    let tempSum = 0;
+    let maxSum = 0;
+    for (var i = 0; i < n; i++){
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+
+    for(var i = n; i < arr.length; i++){
+        tempSum = tempSum - arr[i - n] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+
+
+console.log(maxSubarraySum([1,2,5,2,8,1,5], 2));
+console.log(maxSubarraySum([1,2,5,2,8,1,5], 4));
+console.log(maxSubarraySum([4,2,1,6], 1));
+console.log(maxSubarraySum([4,2,1,6,2], 4));
+console.log(maxSubarraySum([], 4));
