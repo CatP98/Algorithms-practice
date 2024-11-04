d7abe5eb3f73bb0b7b713b5c2a46c77ddb2137c1
catarina p <catarinampestana@ua.pt>
2024-11-04 10:27:00 +0000

    Recursion : isPalindrome

---
+++ algorithms/practices/resources/patterns/recursion/isPalindrome.js
@@ -0,0 +1,29 @@
+/*
+Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome
+(reads the same forward and backward). Otherwise it returns false.
+
+Examples:
+isPalindrome('awesome') // false
+isPalindrome('foobar') // false
+isPalindrome('tacocat') // true
+isPalindrome('amanaplanacanalpanama') // true
+isPalindrome('amanaplanacanalpandemonium') // false
+
+ */
+
+function isPalindrome(str){
+     // Base Case
+    if(str.length <= 1) return true; // because an empty or single-character string is a palindrome
+    // Recursive Case
+    //return helper(isPalindrome(str[0], str[str.length -1])) + isPalindrome(str.slice(1, str.length -2));
+    if(str[0] === str[str.length -1]){
+        return isPalindrome(str.slice(1,-1)); // slice(1,-1) removes the outer characters
+    }
+    else {return false;}
+}
+
+console.log(isPalindrome('awesome'));// false
+console.log(isPalindrome('foobar')); // false
+console.log(isPalindrome('tacocat'));// true
+console.log(isPalindrome('amanaplanacanalpanama')); // true
+console.log(isPalindrome('amanaplanacanalpandemonium')); // false
